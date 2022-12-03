1ba89eb08ef8356bc60ddc7bd35de0aae3b83a35
Ali Habib <edualihabib@gmail.com>
2022-12-03 04:21:19 +0300

    creat header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+// start nav par
+
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click",() => links.classList.toggle("show-links"));
+
+// end nav par
